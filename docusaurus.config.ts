9acd6f65dc8c13b5a46011817665e3159ddf18e2
seirang <seirang.park@gmail.com>
2025-04-26 13:15:06 +0900

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -71,7 +71,7 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'TIL',
+      title: 'Seirang\'s TIL',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
